b0a4e92503e7a0402146b6798d7b929d5e3cbb7d
maggielin18 <maggie.lin@stern.nyu.edu>
2023-08-10 17:12:49 -0400

    skeleton v1

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import { NavBarElement } from "./NavBarElement";
+import '../styles/NavBarStyle.css';
+
+class NavBar extends Component{
+    render(){
+        return(
+            <nav className="navBar">
+                <ul>
+                {NavBarElement.map((key, index) => {
+                    return (
+                        <li
+                            key = {key}
+                            className = 'navItem'
+                            onClick = {() => {
+                                window.location.pathname = key.link;
+                            }}
+                        >
+                            <div id = 'icon'>
+                                {key.icon}
+                            </div>
+                            <div id = 'title'>
+                                {key.title}
+                            </div>
+                        </li>
+                    );
+                })}
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default NavBar;
---
+++ src/components/NavBarElement.js
@@ -0,0 +1,8 @@
+export const NavBarElement = [
+    {
+        title: 'dashboard',
+        icon: <img src={require('../assets/dashboard.png')} alt="" />,
+        link: '/home'
+    },
+];
+
