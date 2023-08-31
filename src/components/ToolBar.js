65f97e50d8dd7ecd84c645f5d642ceee976ff29b
maggielin18 <maggielin187@gmail.com>
2023-08-31 14:00:01 -0400

    navbar/toolbar

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import '../styles/ToolBar.css';
-import { ToolBarElement } from './ToolBarElement';
+import { Component } from 'react';
 
-const ToolBar = () => {
+import { ToolBarElement } from './ToolBarElement';
+import '../styles/ToolBar.css';
+
+class ToolBar extends Component{
+  render(){
     return(
-        <div className='toolBar'>
-            {ToolBarElement.map(element => (
-                <div className='toolBarIcons' key={element.id}>
-                    {element.icon}
-                </div>
-            ))}
-        </div>
+      <div className='toolBar'>
+        {ToolBarElement.map(element => (
+          <div className='toolBarIcons' key={element.id}>
+              {element.icon}
+          </div>
+        ))}
+      </div>
     )
+    // return(
+    //     <nav className="toolBar">
+    //         <ul>
+    //         {ToolBarElement.map((key, index) => {
+    //             return (
+    //                 <li
+    //                     key = {key}
+    //                     className = 'toolBar'
+    //                     onClick = {() => {
+    //                         window.location.pathname = key.link;
+    //                     }}
+    //                 >
+    //                     <div id = 'icon'>
+    //                         {key.icon}
+    //                     </div>
+    //                 </li>
+    //             );
+    //         })}
+    //         </ul>
+    //     </nav>
+    // )
+  }
 }
 
 export default ToolBar;
