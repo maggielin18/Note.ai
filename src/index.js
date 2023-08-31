f9a3d831ce9c2bec6538ac36fbc2f060222338be
sid-chava <siddarth.chava@gmail.com>
2023-08-31 14:06:21 -0400

    index js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import App from './App/App.js';
-import SplashScreen from './screens/SplashScreen.js';
+// import SplashScreen from './screens/SplashScreen.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path='/' element={<App />} />
-                <Route path='/' element={<SplashScreen />} />
-            </Routes>
-        </Router>
-    </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path='/' element={<App />} />
+                {/* <Route path='/' element={<SplashScreen />} /> */}
+            </Routes>
+        </Router>
+    </React.StrictMode>
 );
+
 
 
 
